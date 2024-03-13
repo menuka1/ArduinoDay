59ca61b51844dff34198ffa7b51eaf91237b8444
daraya99 <wtmsilva@students.nsbm.ac.lk>
2024-03-13 10:19:04 +0530

    SPONSOR LOHO

--- src/components/Swag.tsx
+++ src/components/Swag.tsx
@@ -7,7 +7,7 @@ const Swag = ({ }: Props) => {
   return (
     <div id="swag" className="my-20 md:my-8 md:pt-28 md:pb-36 relative">
       <div className='bg-70 h-[95%] md:h-[43%] lg:h-[50%] xl:h-[53%] w-full -z-50 absolute top-[8%] md:top-[24%] lg:top-[25%] xl:top-[23%]'></div>
-      <div className='px-[15%] md:px-[8.33%] grid md:grid-flow-col gap-2'>
+      <div className='px-[10%] md:px-[8.33%] grid md:grid-flow-col gap-2'>
         <div className='place-items-center shrink md:w-80 lg:w-full'>
           <img src="./swagTee.png" alt="arduinoTshirt" className='' />
         </div>
